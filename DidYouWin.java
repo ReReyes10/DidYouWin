40976f62b35307f7acf686c25fe6bf997d424d68
ReReyes10 <reginaldreyes93@gmail.com>
2016-09-19 22:35:44 -0700

    Update DidYouWin.java

--- DidYouWin.java
+++ DidYouWin.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.Random;
 
 public class DidYouWin { 
    
@@ -9,25 +8,24 @@ public class DidYouWin {
         DidYouWin.setTitle("Did You Win?");
         DidYouWin.setSize(900,200);
         DidYouWin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            JPanel panel1 = new JPanel();
-            panel1.setBackground(Color.red);
-            JPanel panel2 = new JPanel();
-            panel2.setBackground(Color.blue);
-            JPanel panel3 = new JPanel();
-            panel3.setBackground(Color.green);
-            JPanel panel4 = new JPanel();
-            panel4.setBackground(Color.blue);
-            JPanel panel5 = new JPanel();
-            panel5.setBackground(Color.red);
-            Container pane = DidYouWin.getContentPane();
-            pane.setLayout(new GridLayout(1,5));
-            pane.add(panel1);
-            pane.add(panel2);
-            pane.add(panel3);
-            pane.add(panel4);
-            pane.add(panel5);
-            Random gen = new Random();            int blue = gen.nextInt(1);            Color backColor = new Color (blue);            //Panel panel = new Panel();            //panel.setBackground(backColor);            ColorPanel panel = new ColorPanel(backColor);            pane.add(panel);
-            DidYouWin.setVisible(true);
+        JPanel panel1 = new JPanel();
+        panel1.setBackground(Color.red);
+        JPanel panel2 = new JPanel();
+        panel2.setBackground(Color.blue);
+        JPanel panel3 = new JPanel();
+        panel3.setBackground(Color.green);
+        JPanel panel4 = new JPanel();
+        panel4.setBackground(Color.blue);
+        JPanel panel5 = new JPanel();
+        panel5.setBackground(Color.red);
+        Container pane = DidYouWin.getContentPane();
+        pane.setLayout(new GridLayout(1,5));
+        pane.add(panel1);
+        pane.add(panel2);
+        pane.add(panel3);
+        pane.add(panel4);
+        pane.add(panel5);
+        DidYouWin.setVisible(true);
     }
 
 }
